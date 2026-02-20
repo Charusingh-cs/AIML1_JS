3d992441867f66c9bd05a9c567b93beb14ed9bdb
Charusingh-cs <charusandhu272@gnail.com>
2026-02-20 14:08:23 +0530

    jvascript basics

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-console.log("apple")
-document.write("hello world" );
+var num = 15;
+console.log(typeof num);   // number
+
+var p = "wahguru";
+console.log(typeof p);     // string
+
+const sym2 = Symbol(40);
+const sym1 = Symbol(4);
+
+if (sym1 === sym2) {
+  console.log("true");
+} else {
+  console.log("false");
+}
+var a=100;
+var b=20;
+var c=300;
+console.log(a+b+c)
