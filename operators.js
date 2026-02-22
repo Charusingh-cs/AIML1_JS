b9f4973b661c8b90ef335493570423f971b96c6b
Charusingh-cs <charusandhu272@gnail.com>
2026-02-22 17:24:43 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,36 @@
+let a=2;
+let b=3;
+//arithmetic operators
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a%b",a%b);
+console.log("a/b",a/b);
+//unary operator
+console.log(a--);
+console.log(a);
+console.log(--a);
+console.log(a++);
+console.log(++a);
+console.log("a=",a);
+//assignment operator
+b**=2;
+
+console.log(b);
+//comparison operators
+//equal to ==
+//equal to and type ===
+//not equal to !=
+//not equalto and type !==
+let c=5;
+let d="5";
+console.log(c!=d);
+console.log(c!==d);
+//logical operators
+//decision of comparison of multiple operations
+//&&
+//||
+//?
+let cond1=(a!=b);
+let cond2=(c==d);
+console.log(cond1&&cond2);
