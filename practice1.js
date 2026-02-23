654da0bcd7c8979519fa86eac3cbf8574639b0d0
Charusingh-cs <charusandhu272@gnail.com>
2026-02-23 12:54:22 +0530

    practice1

---
+++ practice1.js
@@ -0,0 +1,16 @@
+let score=prompt("enter the score");
+if (score>=90){
+    console.log("A");
+}else if(score>=80&&score<90){
+    console.log("B");
+
+}else if(score>=70&&score<8){
+    console.log("C");
+}else if(score>=60&&score<70){
+    console.log("D");
+
+}else if(score>=50&&score<50){
+    console.log("E");
+}else{
+    console.log("less than E");
+}
