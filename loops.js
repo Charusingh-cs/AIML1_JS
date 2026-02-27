57d104e2cb30179a52e22d61d3649ccf4a92fbb0
Charusingh-cs <charusandhu272@gnail.com>
2026-02-27 08:57:45 +0530

    javascript

---
+++ loops.js
@@ -0,0 +1,12 @@
+// for (key in object){
+
+
+// }
+const person={
+    fname:"Ajay",
+    lname:"Singh",
+    age :25
+}
+for(let x in person){
+    console.log("person details:" +x+" "+person[x])
+}
