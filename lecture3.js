933f15e5c62026714646e196c6c8268e2c033221
Charusingh-cs <charusandhu272@gnail.com>
2026-03-06 09:07:12 +0530

    lecture3

---
+++ lecture3.js
@@ -0,0 +1,56 @@
+// const person={
+//     fname:"Ajay",
+//     lname:"Singh",
+//     age:25
+
+// };
+// for(let x in person){
+
+// }
+// 
+//default will execute in last irrespective of position
+// var age=17;
+// if(age>18){
+//     document.write("<br>You are eligible to cast the vote</br>");
+
+// }else{
+//     document.write("minor");
+
+// }
+// var grade='A';
+// document.write("Entering in the block");
+// switch(grade){
+//     case 'A':document.write("Good job");
+//     break;
+//     case 'B':document.write("Pretty good job");
+//     break;
+//     case 'C':document.write("good");
+//     break;
+//     default:document.write("hello");
+// }
+
+// function myfunction(){
+//     alert("hello world");
+// }
+
+//Undefined is used as no return type
+//declaration 
+//defination
+//call
+//symbol used for unique identification 
+//for variable in obect acting as key 
+//methods are functions
+var res=mull(12,30);
+function mull(x,y){
+    return x*y;
+}
+document.write(res);
+function add(a,b){
+    console.log(a+b);
+}
+add(10,20)
+var add2=(a,b)=>{
+    console.log(a+b);
+
+}
+add2(10,20);
