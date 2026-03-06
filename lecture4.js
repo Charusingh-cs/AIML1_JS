81681fb2a64e1e8d5c54d2c5a566ec114d5aeeb6
Charusingh-cs <charusandhu272@gnail.com>
2026-03-06 09:11:54 +0530

    lecture4

---
+++ lecture4.js
@@ -0,0 +1,22 @@
+// var res=mull(12,30);
+// function mull(x,y){
+//     return x*y;
+// }
+// document.write(res);
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(10,20)
+// var add2=(a,b)=>{
+//     console.log(a+b);
+// }
+// add2(10,20);
+var name={
+    name:"john max",
+    age:20,
+    ismarried:true,
+    address:{
+        street:"vijay nagar",
+        flatNo:20}
+    };
+console.log(person);
