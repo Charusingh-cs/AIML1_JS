5a88b3f3911f410b9c55ee93cb4fdd435a60d838
Charusingh-cs <charusandhu272@gnail.com>
2026-03-09 16:12:50 +0530

    object

---
+++ object.js
@@ -0,0 +1,32 @@
+var myinfo = new Array();
+var myinfo1 = Array();
+console.log(myinfo, myinfo1);
+
+let collection = [
+    {}, 
+    [], 
+    true, 
+    "john", 
+    function () {}, 
+    90009898193, 
+    undefined, 
+    null, 
+    new String("abc"), 
+    new Date()
+];
+
+console.log(collection);
+
+collection.teacherName = "John Mac";
+collection.phoneNo = 950008193;
+
+console.log(collection);
+
+var traininfo = ["jitendra", 29, "ghaziabad", { ismarried: true }];
+
+console.log(traininfo[0]);
+console.log(traininfo[1]);
+
+traininfo.phone = 1234455;
+
+console.log(traininfo);
