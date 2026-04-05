196960fbffb1642063e6499894d06a94b7045d7a
Charusingh-cs <charusandhu272@gnail.com>
2026-04-06 00:22:56 +0530

    arraypractice

---
+++ arraypractice.js
@@ -0,0 +1,6 @@
+let company=['bloomberg','Microsoft','Uber','Google','IBM','Netflix'];
+console.log(company.splice(2,1,"ola"));
+console.log(company);
+company.push("Amazon");
+console.log(company);
+
